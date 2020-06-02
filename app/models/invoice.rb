263bef2b87372b3ca374b8716f1fbfb3f8e4112e
charles-henri poniard <c.poniard@gmail.com>
2020-06-02 10:10:08 +0200

    fix bug in test and add require faker

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,15 +8,15 @@ class Invoice < ApplicationRecord
   validates :description, presence: true, length: { minimum: 50 },
     unless: Proc.new { |a| a.description.blank? }
   validates :end_date, presence: true
-  validate :end_date_cant_be_in_the_future
+  validate :end_date_cant_be_in_the_past
   validates :status, presence: true, numericality: {only_integer: true}
   validates :status, inclusion: { in: [0, 1, 2, 3, 4], message: "status doit être entre 0 et 4" }
 
   private
 
-  def end_date_cant_be_in_the_future
-    if end_date.present? && end_date > Date.today
-      errors.add(:end_date, "ne peut pas être dans le future")
+  def end_date_cant_be_in_the_past
+    if end_date.present? && end_date < Date.today
+      errors.add(:end_date, "ne peut pas être dans le passé")
     end
   end
 end
