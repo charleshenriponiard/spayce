37138a130c9176fac0436facc9011e90ea75f3db
charles-henri poniard <c.poniard@gmail.com>
2020-06-10 17:20:01 +0200

    new end create action ok

--- app/controllers/mangopay_users_controller.rb
+++ app/controllers/mangopay_users_controller.rb
@@ -1,16 +1,15 @@
 class MangopayUsersController < ApplicationController
-
   def new
     @mp_user = MangoPay::NaturalUser.new()
   end
 
   def create
     @mp_user = MangoPay::NaturalUser.create(params_formated)
-    until @mp_user.blank?
-      redirect_to root_path
-    else
-      render :new
-    end 
+    # unless @mp_user.nil?
+    #   redirect_to root_path
+    # else
+    #   render :new
+    # end 
   end
 
   private
@@ -21,25 +20,23 @@ class MangopayUsersController < ApplicationController
       :Email,
       :FirstName,
       :LastName,
-      :Address,
-      :AddressLine1,
-      :AddressLine2,
-      :City,
-      :Region,
-      :PostalCode,
-      :Country,
       :Birthday,
       :BirthPlace,
       :Nationality,
-      :CountryOfRestsidance,
+      :CountryOfResidence,
       :Occupation,
-      :InComeRange
+      :InComeRange,
+      :AddressLine1, 
+      :AddressLine2, 
+      :City, 
+      :Region, 
+      :PostalCode,
+      :Country
     )
   end
 
   def params_formated
     params = mangopay_params
-    byebug
     return {
       Tag: params["Tag"],
       Email: params["Email"],
@@ -53,13 +50,23 @@ class MangopayUsersController < ApplicationController
         PostalCode: params["PostalCode"],
         Country: params["Country"]
       },
-      Birthday: Date.parse('01-01-2000').to_time.to_i,
-      Birthplace: 'Paris',
-      Nationality: 'FR',
-      CountryOfResidence: 'FR',
-      Occupation: 'Worker',
-      IncomeRange: 1
+      Birthday: Date.parse('01-01-1999').to_time.to_i,
+      Birthplace: params["Birthday"],
+      Nationality: params["Nationality"],
+      CountryOfResidence: params["CountryOfResidence"],
+      Occupation: params["Occupation"],
+      IncomeRange: params["IncomeRange"]
     }
-  end 
-  
+  end
 end 
+
+
+
+# params[:Address] = {
+#   AddressLine1: params["AddressLine1"],
+#   AddressLine2: params["AddressLine2"],
+#   City: params["City"],
+#   Region: params["Region"],
+#   PostalCode: params["PostalCode"],
+#   Country: params["Country"]
+# }
